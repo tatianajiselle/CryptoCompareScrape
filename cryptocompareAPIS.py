78980fc9ceacbae6b1c7bf46d6cb7d04d086a3d1
tatianajiselle <Tatiana@Tatianas-MacBook-Pro.local>
2018-01-09 23:19:10 -0800

    Added logic to main for API call. Updated response as json object.

--- cryptocompareAPIS.py
+++ cryptocompareAPIS.py
@@ -1,18 +1,39 @@
 import requests
+import json
 
+# save data to appendable file
 def saveToFile(data):
 	f = open('cryptoCompareData.txt', 'a')
-	f.write(data)
+	f.write(str(data))
 	return
 
-def historicalHour(): #Call cryptocompare api for historical hour data
-	r = requests.get('https://min-api.cryptocompare.com/data/histohour?fsym=ETH&tsym=BTC&limit=60&aggregate=1&toTs=1514797200')
-	print (r.text)
-	saveToFile(r.text)
-	return
+# Call cryptocompare api for historical hour data
+# toTs: timestamp is the to date in epoch time
+def historicalHour(toTs): 
+	r = requests.get('https://min-api.cryptocompare.com/data/histohour?fsym=ETH&tsym=BTC&limit=60&aggregate=1&toTs=' + toTs).json()
+	print (r)
+	saveToFile(r)
+	date = r['TimeFrom']
+	return date
 
+# Starts the historical date with hardcoded date. As long as
+# the date is not before june 1, 2017 (endTime), it will call 
+# the api.
 def main(): 
-	historicalHour();
+
+	# end histrical data date
+	# june 1, 2017 0:00:00 1496275200
+	endTime = 1496275200
+
+	#start historical data date
+	# jan 10, 2018 0:00:00 1515542400
+	startTime = 1515542400	
+	
+	date = historicalHour(str(startTime))
+
+	while (int(date) >= endTime):
+		date = historicalHour(str(date))
+
 
 if __name__ == "__main__":# main()
     import sys
