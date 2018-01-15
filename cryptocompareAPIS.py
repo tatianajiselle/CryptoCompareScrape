aabf8aad14f29087d906d6e601b1ded6d94bdce7
tatianajiselle <tjexoxo@gmail.com>
2018-01-14 19:58:42 -0800

    Refactored. Added in HistoDay function

--- cryptocompareAPIS.py
+++ cryptocompareAPIS.py
@@ -3,40 +3,55 @@ import json
 
 # save data to appendable file
 def saveToFile(data):
-	f = open('cryptoCompareData.txt', 'a')
-	f.write(str(data))
-	return
+    f = open('cryptoCompareData.txt', 'a')
+    f.write(str(data))
+    f.write('\ln')
+    return
 
 # Call cryptocompare api for historical hour data
 # toTs: timestamp is the to date in epoch time
 # date is the returned TimeFrom (latest date fetched)
 # BTC and ETH are fetched
-def histoHour(toTs): 
-	r = requests.get('https://min-api.cryptocompare.com/data/histohour?fsym=ETH&tsym=BTC&limit=60&aggregate=1&toTs=' + toTs).json()
-	print (r)
-	saveToFile(r)
-	date = r['TimeFrom']
-	return date
+def histoHour(toTs):
+    r = requests.get(
+        'https://min-api.cryptocompare.com/data/histohour?fsym=ETH&tsym=BTC&limit=60&aggregate=1&toTs=' + toTs).json()
+    print(r)
+    saveToFile(r)
+    return r['TimeFrom']
+
+# Call cryptocompare api for historical day data
+# toTs: timestamp is the to date in epoch time
+# date is the returned TimeFrom (latest date fetched)
+# BTC and ETH are fetched
+def histoDay(toTs):
+    r = requests.get(
+        'https://min-api.cryptocompare.com/data/histoday?fsym=ETH&tsym=BTC&limit=60&aggregate=1&toTs=' + toTs).json()
+    print(r)
+    saveToFile(r)
+    return r['TimeFrom']
 
 # Starts the historical date with hardcoded date. As long as
-# the date is not before june 1, 2017 (endTime), it will call 
+# the date is not before june 1, 2017 (endTime), it will call
 # the api.
-def main(): 
+def main():
 
-	# end historical data date
-	# june 1, 2017 0:00:00 1496275200
-	endTime = 1496275200
+    # end historical data date
+    # june 1, 2017 0:00:00 1496275200
+    endTime = 1496275200
 
-	#start historical data date
-	# jan 10, 2018 0:00:00 1515542400
-	startTime = 1515542400	
+    # start historical data date
+    # jan 10, 2018 0:00:00 1515542400
+    startTime = 1515542400
 
-	date = histoHour(str(startTime))
+    # date = histoHour(str(startTime))
+    # while (int(date) >= endTime):
+    # 	date = histoHour(str(date))
 
-	while (int(date) >= endTime):
-		date = histoHour(str(date))
+    date = histoDay(str(startTime))
+    while (int(date) >= endTime):
+    	date = histoDay(str(date))
 
 
-if __name__ == "__main__":# main()
+if __name__ == "__main__":  # main()
     import sys
     main()
