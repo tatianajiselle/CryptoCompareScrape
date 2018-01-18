73ed2b05225bb9fa686e4530d94b8728987510c4
tatianajiselle <tjexoxo@gmail.com>
2018-01-17 19:55:13 -0800

    Refactored functions. added two additional conversion ratios. Uploaded data.

--- cryptocompareAPIS.py
+++ cryptocompareAPIS.py
@@ -3,32 +3,73 @@ import json
 
 # save data to appendable file
 def save_to_file(data):
-    f = open('cryptoCompareData.txt', 'a')
+    f = open('CC_HistoDay_BTC_ETH.txt', 'a')
     f.write(str(data))
-    f.write('\ln')
+    f.write('\n')
     return
 
-# Call cryptocompare api for historical hour data
-# toTs: timestamp is the to date in epoch time
-# date is the returned TimeFrom (latest date fetched)
-# BTC and ETH are fetched
-def histo_hour(to_ts):
+#                                                         # 
+#               ETH to BTC are fetched                    #
+#                                                         #
+# toTs: timestamp is the to date in epoch time            #
+# date is the returned TimeFrom (latest date fetched)     #
+
+# Historical hour data 
+def histo_hour_eth_btc(to_ts):
     r = requests.get(
         'https://min-api.cryptocompare.com/data/histohour?fsym=ETH&tsym=BTC&limit=60&aggregate=1&toTs=' + to_ts).json()
     print(r)
     save_to_file(r)
     return r['TimeFrom']
 
-# Call cryptocompare api for historical day data
-# toTs: timestamp is the to date in epoch time
-# date is the returned TimeFrom (latest date fetched)
-# BTC and ETH are fetched
-def histo_day(to_ts):
+# Historical day data
+def histo_day_eth_btc(to_ts):
     r = requests.get(
         'https://min-api.cryptocompare.com/data/histoday?fsym=ETH&tsym=BTC&limit=60&aggregate=1&toTs=' + to_ts).json()
     print(r)
     save_to_file(r)
     return r['TimeFrom']
+
+#                                                         # 
+#               BTC to ETH are fetched                    #
+#                                                         #
+# toTs: timestamp is the to date in epoch time            #
+# date is the returned TimeFrom (latest date fetched)     #
+
+def histo_hour_btc_eth(to_ts):
+    r = requests.get(
+        'https://min-api.cryptocompare.com/data/histohour?fsym=BTC&tsym=ETH&limit=60&aggregate=1&toTs=' + to_ts).json()
+    print(r)
+    save_to_file(r)
+    return r['TimeFrom']
+
+def histo_day_btc_eth(to_ts):
+    r = requests.get(
+        'https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=ETH&limit=60&aggregate=1&toTs=' + to_ts).json()
+    print(r)
+    save_to_file(r)
+    return r['TimeFrom']
+
+#                                                         # 
+#               BTC to USD are fetched                    #
+#                                                         #
+# toTs: timestamp is the to date in epoch time            #
+# date is the returned TimeFrom (latest date fetched)     #
+
+def histo_day_btc_usd(to_ts):
+    r = requests.get(
+        'https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=60&aggregate=1&toTs=' + to_ts).json()
+    print(r)
+    save_to_file(r)
+    return r['TimeFrom']
+
+def histo_hour_btc_usd(to_ts):
+    r = requests.get(
+        'https://min-api.cryptocompare.com/data/histohour?fsym=BTC&tsym=USD&limit=60&aggregate=1&toTs=' + to_ts).json()
+    print(r)
+    save_to_file(r)
+    return r['TimeFrom']
+
 
 # Starts the historical date with hardcoded date. As long as
 # the date is not before june 1, 2017 (endTime), it will call
@@ -43,13 +84,35 @@ def main():
     # jan 10, 2018 0:00:00 1515542400
     startTime = 1515542400
 
-    # date = histo_hour(str(startTime))
+    # HOURLY ETH to BTC
+    # date = histo_hour_eth_btc(str(startTime))
     # while (int(date) >= endTime):
-    # 	date = histo_hour(str(date))
+    # 	date = histo_hour_eth_btc(str(date))
 
-    date = histo_day(str(startTime))
+    # DAILY ETH to BTC
+    # date = histo_day_eth_btc(str(startTime))
+    # while (int(date) >= endTime):
+    # 	date = histo_day_eth_btc(str(date))
+
+    # HOURLY BTC to USD
+    # date = histo_day_btc_usd(str(startTime))
+    # while (int(date) >= endTime):
+    #     date = histo_day_btc_usd(str(date))
+
+    # DAILY BTC to USD
+    # date = histo_hour_btc_usd(str(startTime))
+    # while (int(date) >= endTime):
+    #     date = histo_hour_btc_usd(str(date))
+
+    # HOURLY BTC to ETH
+    # date = histo_hour_btc_eth(str(startTime))
+    # while (int(date) >= endTime):
+    #     date = histo_hour_btc_eth(str(date))
+
+    # DAILY BTC to ETH
+    date = histo_day_btc_eth(str(startTime))
     while (int(date) >= endTime):
-    	date = histo_day(str(date))
+        date = histo_day_btc_eth(str(date))
 
 
 if __name__ == "__main__":  # main()
