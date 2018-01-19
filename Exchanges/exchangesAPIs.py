a9e7d8a11e72a81b43ed55165e68b3817dc379a6
tatianajiselle <tjexoxo@gmail.com>
2018-01-18 18:29:07 -0800

    added binance and bittrex api + data

--- Exchanges/exchangesAPIs.py
+++ Exchanges/exchangesAPIs.py
@@ -8,24 +8,46 @@ import requests
 import json
 
 # save data to appendable file
-def save_to_file(data):
-    f = open('Poloniex/BTC_XMR_4H.txt', 'a')
+def save_to_file(exchange, data, trading_pair):
+    f = open(exchange + '/'+ trading_pair, 'a')
     f.write(str(data))
-    f.write('\n')
     return
 
 
 # start = 1498867200 July 1, 2017 00:00:00
 # end = year 2286
-def poloniex():
-    r = requests.get('https://poloniex.com/public?command=returnChartData&currencyPair=BTC_XMR&start=1405699200&end=9999999999&period=14400').json()
+def poloniex(trading_pair):
+    r = requests.get('https://poloniex.com/public?command=returnChartData&currencyPair='
+                     + trading_pair
+                     + '&start=1405699200&end=9999999999&period=14400'
+                     ).json()
     print(r)
-    save_to_file(r)
+    save_to_file('Poloniex', r, trading_pair)
+    return
+
+
+def binance():
+    r = requests.get('https://api.binance.com/api/v1/klines').json()
+    print(r)
+    save_to_file('Binance', r, 'Binance_Kline')
+    return
+
+def bittrex(trading_pair,time_interval):
+    r = requests.get('https://bittrex.com/Api/v2.0/pub/market/GetTicks?marketName='
+                     + trading_pair
+                     + '&tickInterval='
+                     + time_interval
+                     + '&_=1499127220008').json()
+    print(r)
+    save_to_file('Bittrex', r, trading_pair + '_' + time_interval)
     return
 
 def main():
 
-    poloniex();
+ poloniex('BTC_ETH');
+  binance();
+    bittrex('BTC-WAVES', 'oneMin');
+    bittrex('BTC-WAVES', 'thirtyMin');
 
 
 if __name__ == "__main__":  # main()
