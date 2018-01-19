3415c00345c02844a4d036ab5575045acb4b468a
tatianajiselle <tjexoxo@gmail.com>
2018-01-18 23:13:15 -0800

    Updated for Kraken api + data

--- Exchanges/exchangesAPIs.py
+++ Exchanges/exchangesAPIs.py
@@ -4,8 +4,10 @@
 # By: Tatiana Ensslin
 # Exchanges scraped: Poloniex, gdax, binance, bittrex, kraken, and gemini
 
+
 import requests
 import json
+
 
 # save data to appendable file
 def save_to_file(exchange, data, trading_pair):
@@ -14,6 +16,7 @@ def save_to_file(exchange, data, trading_pair):
     return
 
 
+# POLONIEX
 # start = 1498867200 July 1, 2017 00:00:00
 # end = year 2286
 def poloniex(trading_pair):
@@ -26,29 +29,45 @@ def poloniex(trading_pair):
     return
 
 
+# BINANCE
 def binance():
     r = requests.get('https://api.binance.com/api/v1/klines').json()
     print(r)
     save_to_file('Binance', r, 'Binance_Kline')
     return
 
-def bittrex(trading_pair,time_interval):
+
+# BITTREX
+def bittrex(trading_pair, time_interval):
     r = requests.get('https://bittrex.com/Api/v2.0/pub/market/GetTicks?marketName='
                      + trading_pair
                      + '&tickInterval='
                      + time_interval
-                     + '&_=1499127220008').json()
+                     + '&_=1499127220008'
+                     ).json()
     print(r)
     save_to_file('Bittrex', r, trading_pair + '_' + time_interval)
     return
 
+# KRAKEN
+def kraken(trading_pair, time_interval):
+    r = requests.get('https://api.kraken.com/0/public/Trades?pair='
+                     + trading_pair
+                     + '&since='
+                     + time_interval
+                     ).json()
+    print(r)
+    save_to_file('Kraken', r, trading_pair)
+    return
+
+
 def main():
 
-    poloniex('BTC_ETH');
-    binance();
-    bittrex('BTC-WAVES', 'oneMin');
-    bittrex('BTC-WAVES', 'thirtyMin');
-
+#    poloniex('BTC_ETH');
+#    binance();
+#    bittrex('BTC-WAVES', 'oneMin');
+#    bittrex('BTC-WAVES', 'thirtyMin');
+    kraken('XXBTZEUR', '1498867200'); # Saturday, July 1, 2017  init interavl '1383839436659595694' microseconds
 
 if __name__ == "__main__":  # main()
     import sys
